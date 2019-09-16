562118bbd5c71144f21af9388a2dda69dba962cb
Deril Alwinda <deril@yahoo.com>
2019-09-16 14:26:39 +0700

    try catch fetchin cabang

--- ViewModels/SettingsViewModel.cs
+++ ViewModels/SettingsViewModel.cs
@@ -39,34 +39,38 @@ namespace GameLauncher.ViewModels
             public CabangAutoCompleteSource()
             {
                 m_cabangItems = new List<CabangList>();
-                using (WebClient client = new WebClient())
+                try
                 {
-                    //Now get your response from the client from get request to baseurl.
-                    //Use the await keyword since the get request is asynchronous, and want it run before next asychronous operation.
-                    string strJSON = client.DownloadString(baseURL);
-                    var dataObj = JsonValue.Parse(strJSON);
-                    if (dataObj != null)
+                    using (WebClient client = new WebClient())
                     {
-                        //Parse your data into a object.
-                        foreach (JsonObject cabang in dataObj)
+                        //Now get your response from the client from get request to baseurl.
+                        //Use the await keyword since the get request is asynchronous, and want it run before next asychronous operation.
+                        string strJSON = client.DownloadString(baseURL);
+                        var dataObj = JsonValue.Parse(strJSON);
+                        if (dataObj != null)
                         {
-                            CabangList newcabang = new CabangList();
-                            newcabang.Name = cabang["name"];
-                            m_cabangItems.Add(newcabang);
+                            //Parse your data into a object.
+                            foreach (JsonObject cabang in dataObj)
+                            {
+                                CabangList newcabang = new CabangList();
+                                newcabang.Name = cabang["name"];
+                                m_cabangItems.Add(newcabang);
 
+                            }
+                            //Then create a new instance of PokeItem, and string interpolate your name property to your JSON object.
+                            //Which will convert it to a string, since each property value is a instance of JToken.
+                            //Log your pokeItem's name to the Console.
                         }
-                        //Then create a new instance of PokeItem, and string interpolate your name property to your JSON object.
-                        //Which will convert it to a string, since each property value is a instance of JToken.
-                        //Log your pokeItem's name to the Console.
+                        else
+                        {
+                            //If data is null log it into console.
+                            return;
+                        }
                     }
-                    else
-                    {
-                        //If data is null log it into console.
-                        return;
-                    }
-
-
-
+                }
+                catch
+                {
+                    Console.WriteLine("Error fetching cabang");
                 }
             }
 
