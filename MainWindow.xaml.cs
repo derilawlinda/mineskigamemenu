ec3be85bd47c9aaf65d221a4dd57b9e93a3287c2
Deril Alwinda <deril@yahoo.com>
2019-09-18 16:04:13 +0700

    hide onine buttons

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -655,6 +655,7 @@ namespace GameLauncher
         {
             view = "poster";
             DataContext = posterViewModel;
+            this.RefreshGames();
             Settings.Default.viewtype = "Poster";
             Settings.Default.Save();
         }
